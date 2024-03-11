680efe25b883466fe8bbf6bd37977866581b879e
zys8119 <770959294@qq.com>
2024-03-11 16:56:31 +0800

    [add] message

---
+++ serve.ts
@@ -0,0 +1,49 @@
+#!/usr/bin/env ts-node-esm
+import BuildServe, {TransformOptions} from "ts-node-build"
+import {program} from "commander"
+(async ()=>{
+    try {
+        program
+            .showHelpAfterError()
+            .showSuggestionAfterError()
+            .usage("[global options] command")
+            .option("-c, --cwd <path>","当前工作目录", process.cwd())
+            .option("-f, --files <files>","工作目录", '!(node_modules|.git|.idea|.DS_Store|dist|build|unit_test)/**/**')
+            .parse()
+        const options =  program.opts()
+        let result = 0;
+        await new (BuildServe as any).default({
+            isOutInfo:false,
+            inputFiles:[options.files],
+            isOutDir:false,
+            inputFilesOptions:{
+                absolute:true
+            },
+            cwd:options.cwd,
+            rules:[
+                {
+                    rule:/./,
+                    transform({code, file}: TransformOptions): Promise<string | void> | string | void {
+                        if(!/\.(png|svg|ttf|otf|woff|woff2|jpg|jpeg|gif)$/.test(file)){
+                            // console.log(file, code.split('\n').length)
+                            result += code.split('\n').length
+                        }
+                        return code
+                    }
+                }
+            ],
+            onError(error: Error): Promise<any> | void {
+                console.log((this as Error).message)
+            }
+        }).compile()
+        console.log("代码总行数",result)
+        if(Object.keys(program.opts()).length === 0){
+            program.help()
+        }
+    }catch (e:any) {
+        console.error(e)
+        program.help()
+    }
+
+
+})()
